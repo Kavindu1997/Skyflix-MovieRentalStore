140221e8d176aa017dc91b534c4fe7952445ff10
Kavindu1997 <kksamaraweera1997@gmail.com>
2024-03-26 17:41:27 +0530

    Add membership type to list of customers

---
+++ Migrations/20240326120303_AddNameToMembershipType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkyFlix.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNameToMembershipType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "MembershipTypes",
+                type: "longtext",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "MembershipTypes");
+        }
+    }
+}
--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -4,6 +4,8 @@ namespace SkyFlix.Models
     {
         public byte Id { get; set; }
 
+        public string? Name { get; set; }
+
         public short SignUpFee { get; set; }
 
         public byte DurationInMonths { get; set; }

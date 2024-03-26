f2f4cdb75afef0dd8a1f989f179145a0673ca4b0
Kavindu1997 <kksamaraweera1997@gmail.com>
2024-03-26 17:22:11 +0530

    eager loading the membership discount

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SkyFlix.Data;
 using SkyFlix.Models;
 
@@ -18,7 +19,7 @@ namespace SkyFlix.Controllers
         }
         public ViewResult Index()
         {
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }

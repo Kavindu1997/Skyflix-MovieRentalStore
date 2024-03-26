4a7376d4069825e6f2c7386b3d3b289f52097e9a
Kavindu1997 <kksamaraweera1997@gmail.com>
2024-03-26 14:27:04 +0530

    Apply annotations to customer name

---
+++ Migrations/20240326085614_ApplyAnnotationsToCustomerName.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkyFlix.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplyAnnotationsToCustomerName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsSubscribedToNewsLetter",
+                table: "Customers",
+                type: "tinyint(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsSubscribedToNewsLetter",
+                table: "Customers",
+                type: "tinyint(1)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(255)",
+                oldMaxLength: 255);
+        }
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SkyFlix.Models
 {
-    public class Customer{
+    public class Customer
+    {
         public int Id { get; set; }
         public string? Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType? MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }

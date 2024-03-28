1a38ff204df518304aca2b5fd1107043011bec1c
Kavindu1997 <kksamaraweera1997@gmail.com>
2024-03-28 15:32:20 +0530

    Adding validation

---
+++ Models/Min18YearsIfAMember.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using SkyFlix.Models;
+
+namespace SkyFlix.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+                return ValidationResult.Success!;
+
+            if (customer.BirthDate == null)
+                return new ValidationResult("Birthdate is required.");
+
+            var age = DateTime.Today.Year - customer.BirthDate.Value.Year;
+
+            return (age >= 18) ? ValidationResult.Success!
+                        : new ValidationResult("Customer should be at least 18 years old to go on a membership.");
+        }
+    }
+}

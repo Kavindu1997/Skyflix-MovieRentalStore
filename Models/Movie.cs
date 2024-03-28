5fb7d6c35772a4c90fbfc4b28c52c1c2678ec9cc
Kavindu1997 <kksamaraweera1997@gmail.com>
2024-03-28 12:39:13 +0530

    Add a form to create/edit movie

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -11,12 +11,17 @@ namespace SkyFlix.Models
         public string? Name { get; set; }
 
         public Genre? Genre { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
 
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
     }
 }
---
+++ ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,22 @@
+using SkyFlix.Models;
+
+namespace SkyFlix.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        public IEnumerable<Genre>? Genres { get; set; }
+        public Movie? Movie { get; set; }
+
+        public string Title
+        {
+            get
+            {
+                if (Movie != null && Movie.Id != 0)
+                    return "Edit Movie";
+
+                return "New Movie";
+            }
+        }
+    }
+
+}

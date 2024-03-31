d4fda345cc0648245d9452268566373864e910a7
Kavindu1997 <kksamaraweera1997@gmail.com>
2024-03-31 19:19:46 +0530

    Feed DataTable with customers API

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -20,9 +20,10 @@ namespace SkyFlix.Controllers
         }
         public ViewResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            // var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
-            return View(customers);
+            // return View(customers);
+            return View();
         }
 
         public ActionResult Details(int id)
